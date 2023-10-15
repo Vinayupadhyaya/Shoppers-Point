84cfe68f120ca284f7ec780152274a494291778f
vinay <vinayupadhyay1011@gmail.com>
2023-10-16 01:30:32 +0530

    creating the create account page

--- frontend/src/createaccount/Createaccount.js
+++ frontend/src/createaccount/Createaccount.js
@@ -1,15 +1,39 @@
 import React from "react";
-
+// import axios from "axios";
+import { useState } from "react";
 function Createaccount() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  function saveUser() {
+    console.warn(name, email, password);
+    let data = { name, email, password };
+    fetch("http://127.0.0.1:8000/", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    }).then((result) => {
+      result.json().then((resp) => {
+        console.warn("resp", resp);
+      });
+    });
+  }
   return (
     <div>
       <form action="">
         <div class="mb-3">
           <label for="exampleFormControlInput1" class="form-label">
-            Email address
+            Name
           </label>
           <input
             type="text"
+            value={name}
+            onChange={(e) => {
+              setName(e.target.vlaue);
+            }}
             class="form-control"
             id="exampleFormControlInput1"
             placeholder="name"
@@ -21,6 +45,10 @@ function Createaccount() {
           </label>
           <input
             type="email"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.vlaue);
+            }}
             class="form-control"
             id="exampleFormControlInput1"
             placeholder="name@example.com"
@@ -32,11 +60,15 @@ function Createaccount() {
           </label>
           <input
             type="password"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.vlaue);
+            }}
             class="form-control"
             id="exampleInputPassword1"
           />
         </div>
-        <button type="submit" class="btn btn-primary">
+        <button type="submit" onClick={saveUser} class="btn btn-primary">
           Create
         </button>
       </form>
